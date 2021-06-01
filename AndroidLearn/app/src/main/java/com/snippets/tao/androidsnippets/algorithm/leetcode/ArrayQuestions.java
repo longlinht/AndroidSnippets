d252e87b95b539f8e6be0d835b1242e22cb1301e
hetao <hetaoof@gmail.com>
2021-06-01 16:47:26 +0800

    update

--- AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/ArrayQuestions.java
+++ AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/ArrayQuestions.java
@@ -20,22 +20,33 @@ public class ArrayQuestions {
         System.out.println(sb);
     }
 
+    private static void swap(int array[], int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
     public static void test() {
         int b[] = new int[] {1,0,2,3,0,4,5,0};
 
         //int[] nums1 = new int[] {1, 2, 3, 0, 0, 0};
         //int[] nums2 = new int[] {2, 5, 6};
 
-        int[] nums1 = new int[] {2, 0};
-        int[] nums2 = new int[] {1};
+        //int[] nums1 = new int[] {2, 0};
+        //int[] nums2 = new int[] {1};
+
+        //int[] nums = new int[] {0,1,2,2,3,0,4,2};
+        int[] nums = new int[] {3, 2, 2, 3};
 
         System.out.println("before:");
-        printArray(nums1);
+        printArray(nums);
 
-        merge(nums1, 1, nums2, 1);
+        //merge(nums1, 1, nums2, 1);
+        int length = removeElement(nums, 3);
 
         System.out.println("after:");
-        printArray(nums1);
+        printArray(nums);
+        System.out.println("length:" + length);
     }
 
     /**
@@ -150,6 +161,79 @@ public class ArrayQuestions {
             nums1[pointer3] = nums2[pointer2];
             pointer3--;
             pointer2--;
+        }
+    }
+
+    /**
+     * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+     *
+     * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+     *
+     * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+     *
+     * Clarification:
+     *
+     * Confused why the returned value is an integer but your answer is an array?
+     *
+     * Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+     *
+     * Internally you can think of this:
+     *
+     * // nums is passed in by reference. (i.e., without making a copy)
+     * int len = removeElement(nums, val);
+     *
+     * // any modification to nums in your function would be known by the caller.
+     * // using the length returned by your function, it prints the first len elements.
+     * for (int i = 0; i < len; i++) {
+     *     print(nums[i]);
+     * }
+     *
+     *
+     * Example 1:
+     *
+     * Input: nums = [3,2,2,3], val = 3
+     * Output: 2, nums = [2,2]
+     * Explanation: Your function should return length = 2, with the first two elements of nums being 2.
+     * It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
+     * Example 2:
+     *
+     * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+     * Output: 5, nums = [0,1,4,0,3]
+     * Explanation: Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4. Note that the order of those five elements can be arbitrary. It doesn't matter what values are set beyond the returned length.
+     *
+     *
+     * Constraints:
+     *
+     * 0 <= nums.length <= 100
+     * 0 <= nums[i] <= 50
+     * 0 <= val <= 100
+     *
+     */
+    public static int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int n = nums.length;
+        int i = 0;
+        int j = n - 1;
+        while(j > i) {
+            if (nums[i] == val && nums[j] != val) {
+                swap(nums, i, j);
+                i++;
+                j--;
+            } else if (nums[i] != val && nums[j] == val) {
+                i++;
+                j--;
+            }  else if (nums[i] != val && nums[j] != val) {
+                i++;
+            } else if (nums[i] == val && nums[j] == val) {
+                j--;
+            }
+        }
+
+        if(nums[i] != val) {
+            return i + 1;
+        } else {
+            return i;
         }
     }
 }
