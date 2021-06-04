be284e887c8cf1b2b0a1370c94eccdc4bc40fe20
hetao <hetaoof@gmail.com>
2021-06-04 17:41:05 +0800

    update;

--- AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/LinkedListQuestions.java
+++ AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/LinkedListQuestions.java
@@ -53,6 +53,19 @@ public class LinkedListQuestions {
         // myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
         // myLinkedList.get(1);
 
+        ListNode node3 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node0 = new ListNode(0);
+        ListNode noden4 = new ListNode(-4);
+        node3.next = node2;
+        node2.next = node0;
+        node0.next = noden4;
+        noden4.next = node2;
+
+        ListNode begin = detectCycle(node3);
+
+
+        System.out.println("hasCycle: " + begin.val);
     }
 
     /**
@@ -102,6 +115,16 @@ public class LinkedListQuestions {
      * "addAtHead","addAtIndex","addAtHead"]
      * [[],[7],[2],[1],[3,0],[2],[6],[4],[4],[4],[5,0],[6]]
      *
+     *
+     *
+     *
+     *  Your MyLinkedList object will be instantiated and called as such:
+     *  MyLinkedList obj = new MyLinkedList();
+     *  int param_1 = obj.get(index);
+     *  obj.addAtHead(val);
+     *  obj.addAtTail(val);
+     *  obj.addAtIndex(index,val);
+     *  obj.deleteAtIndex(index);
      */
 
     private static class MyLinkedList {
@@ -199,16 +222,209 @@ public class LinkedListQuestions {
     }
 
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
+    /**
+     * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+     *
+     * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+     * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is
+     * connected to. Note that pos is not passed as a parameter.
+     *
+     * Return true if there is a cycle in the linked list. Otherwise, return false.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: head = [3,2,0,-4], pos = 1
+     * Output: true
+     * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+     * Example 2:
+     *
+     *
+     * Input: head = [1,2], pos = 0
+     * Output: true
+     * Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+     * Example 3:
+     *
+     *
+     * Input: head = [1], pos = -1
+     * Output: false
+     * Explanation: There is no cycle in the linked list.
+     *
+     *
+     * Constraints:
+     *
+     * The number of the nodes in the list is in the range [0, 104].
+     * -105 <= Node.val <= 105
+     * pos is -1 or a valid index in the linked-list.
+     *
+     *
+     * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+     *
+     */
+
+      //Definition for singly-linked list.
+     private static class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
 
 
+    public static boolean hasCycle(ListNode head) {
+         if (head == null)
+             return false;
+
+         ListNode fast = head;
+         ListNode slow = head;
+
+         while (fast.next != null && fast.next.next != null) {
+             fast = fast.next.next;
+             slow = slow.next;
+             if (fast == slow)
+                 return true;
+         }
+
+         return false;
+    }
+
+    /**
+     *
+     * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+     *
+     * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+     * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer
+     * is connected to. Note that pos is not passed as a parameter.
+     *
+     * Notice that you should not modify the linked list.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: head = [3,2,0,-4], pos = 1
+     * Output: tail connects to node index 1
+     * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+     * Example 2:
+     *
+     *
+     * Input: head = [1,2], pos = 0
+     * Output: tail connects to node index 0
+     * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+     * Example 3:
+     *
+     *
+     * Input: head = [1], pos = -1
+     * Output: no cycle
+     * Explanation: There is no cycle in the linked list.
+     *
+     *
+     * Constraints:
+     *
+     * The number of the nodes in the list is in the range [0, 104].
+     * -105 <= Node.val <= 105
+     * pos is -1 or a valid index in the linked-list.
+     *
+     *
+     * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+     *
+     */
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode search = head;
+                while(search != slow) {
+                    search = search.next;
+                    slow = slow.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     *
+     * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
+     * If the two linked lists have no intersection at all, return null.
+     *
+     * For example, the following two linked lists begin to intersect at node c1:
+     *
+     *
+     * It is guaranteed that there are no cycles anywhere in the entire linked structure.
+     *
+     * Note that the linked lists must retain their original structure after the function returns.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+     * Output: Intersected at '8'
+     * Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+     * From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2
+     * nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+     * Example 2:
+     *
+     *
+     * Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+     * Output: Intersected at '2'
+     * Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+     * From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes
+     * before the intersected node in A; There are 1 node before the intersected node in B.
+     * Example 3:
+     *
+     *
+     * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+     * Output: No intersection
+     * Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the
+     * two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+     * Explanation: The two lists do not intersect, so return null.
+     *
+     *
+     * Constraints:
+     *
+     * The number of nodes of listA is in the m.
+     * The number of nodes of listB is in the n.
+     * 0 <= m, n <= 3 * 104
+     * 1 <= Node.val <= 105
+     * 0 <= skipA <= m
+     * 0 <= skipB <= n
+     * intersectVal is 0 if listA and listB do not intersect.
+     * intersectVal == listA[skipA + 1] == listB[skipB + 1] if listA and listB intersect.
+     *
+     *
+     * Follow up: Could you write a solution that runs in O(n) time and use only O(1) memory?
+     *
+     *
+     */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode curA = headA, curB = headB;
+        while(curA != curB){
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
+        }
+        return curA;
+    }
 
 }
