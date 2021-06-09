6412fae6af00aab56bddce9f45abf7f408cef67f
hetao <hetaoof@gmail.com>
2021-06-09 15:03:05 +0800

    update;

--- AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/QueueStackQuestions.java
+++ AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/QueueStackQuestions.java
@@ -66,36 +66,88 @@ public class QueueStackQuestions {
 
     class MyCircularQueue {
 
-        final int[] a;
-        int front, rear = -1, len = 0;
 
-        public MyCircularQueue(int k) { a = new int[k];}
+        private int[] data;
+        private int head;
+        private int tail;
+        private int size;
 
-        public boolean enQueue(int val) {
-            if (!isFull()) {
-                rear = (rear + 1) % a.length;
-                a[rear] = val;
-                len++;
-                return true;
-            } else return false;
+        /**
+         * Initialize your data structure here. Set the size of the queue to be k.
+         */
+        public MyCircularQueue(int k) {
+            data = new int[k];
+            head = -1;
+            tail = -1;
+            size = k;
         }
 
+        /**
+         * Insert an element into the circular queue. Return true if the operation is successful.
+         */
+        public boolean enQueue(int value) {
+            if (isFull() == true) {
+                return false;
+            }
+            if (isEmpty() == true) {
+                head = 0;
+            }
+            tail = (tail + 1) % size;
+            data[tail] = value;
+            return true;
+        }
+
+        /**
+         * Delete an element from the circular queue. Return true if the operation is successful.
+         */
         public boolean deQueue() {
-            if (!isEmpty()) {
-                front = (front + 1) % a.length;
-                len--;
+            if (isEmpty() == true) {
+                return false;
+            }
+            if (head == tail) {
+                head = -1;
+                tail = -1;
                 return true;
-            } else return false;
+            }
+            head = (head + 1) % size;
+            return true;
         }
 
-        public int Front() { return isEmpty() ? -1 : a[front];}
+        /**
+         * Get the front item from the queue.
+         */
+        public int Front() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return data[head];
+        }
 
-        public int Rear() {return isEmpty() ? -1 : a[rear];}
+        /**
+         * Get the last item from the queue.
+         */
+        public int Rear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return data[tail];
+        }
 
-        public boolean isEmpty() { return len == 0;}
+        /**
+         * Checks whether the circular queue is empty or not.
+         */
+        public boolean isEmpty() {
+            return head == -1;
+        }
 
-        public boolean isFull() { return len == a.length;}
+        /**
+         * Checks whether the circular queue is full or not.
+         */
+        public boolean isFull() {
+            return ((tail + 1) % size) == head;
+        }
 
     }
+
 
 }
