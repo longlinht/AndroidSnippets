e06e4bca7963c355de24b42776bcc5c1c7b812e4
hetao <hetaoof@gmail.com>
2021-06-16 19:51:20 +0800

    update;

--- AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/QueueStackQuestions.java
+++ AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/QueueStackQuestions.java
@@ -57,8 +57,14 @@ public class QueueStackQuestions {
                 { 0, 0, 0, 0, 0 },
                 { 1, 0, 1, 0, 1 } };
 
-        System.out.println("count: " + numIslands(mx));
+        int mat2[][]  = new int[][]{
+            {0,0,0},
+            {0,1,0},
+            {1,1,1}};
 
+        updateMatrix(mat2);
+
+        System.out.println("count: " + numIslands(mx));
 
 
         String[] deadends = {"0201","0101","0102","1212","2002"};
@@ -1427,7 +1433,35 @@ public class QueueStackQuestions {
      *
      */
     public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
 
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                }
+                else {
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n ||
+                        mat[r][c] <= mat[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[] {r, c});
+                mat[r][c] = mat[cell[0]][cell[1]] + 1;
+            }
+        }
+        return mat;
     }
 
 }
