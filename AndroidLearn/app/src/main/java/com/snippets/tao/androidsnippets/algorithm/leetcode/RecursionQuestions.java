958678216895ee3bcc01e31fc166903e2879641e
hetao <hetaoof@gmail.com>
2021-06-24 10:42:21 +0800

    update;

--- AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/RecursionQuestions.java
+++ AndroidLearn/app/src/main/java/com/snippets/tao/androidsnippets/algorithm/leetcode/RecursionQuestions.java
@@ -456,4 +456,66 @@ public class RecursionQuestions {
         }
         return list;
     }
+
+    /**
+     *
+     * Sort an Array
+     * Given an array of integers nums, sort the array in ascending order.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: nums = [5,2,3,1]
+     * Output: [1,2,3,5]
+     * Example 2:
+     *
+     * Input: nums = [5,1,1,2,0,0]
+     * Output: [0,0,1,1,2,5]
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= nums.length <= 5 * 104
+     * -5 * 104 <= nums[i] <= 5 * 104
+     *
+     *
+     */
+
+    public int[] sortArray(int[] nums) {
+        int N = nums.length;
+        mergeSort(nums, 0, N-1);
+        return nums;
+    }
+
+
+    void mergeSort(int[] nums, int start, int end){
+        if (end - start+1 <= 1) return; //Already sorted.
+        int mi = start + (end - start)/ 2;
+        mergeSort(nums, start, mi);
+        mergeSort(nums, mi+1, end);
+        merge(nums, start,mi, end);
+    }
+
+    void merge(int[] nums, int start, int mi, int end){
+        int lp = start;
+        int rp = mi + 1;
+        int[] buffer = new int[end-start+1];
+        int t = 0; //buffer pointer
+
+        while (lp <= mi && rp <= end){
+            if (nums[lp] < nums[rp]){
+                buffer[t++] = nums[lp++];
+            }else{
+                buffer[t++] = nums[rp++];
+            }
+        }
+
+        while (lp <= mi) buffer[t++] = nums[lp++];
+        while (rp <= end) buffer[t++] = nums[rp++];
+        //Now copy sorted buffer into original array
+        for (int i = start; i <= end; i++){
+            nums[i] = buffer[i-start];
+        }
+    }
 }
